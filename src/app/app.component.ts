b85cb7d7196a8130e66897ff2ae2c8ebbce90585
aaron <aarontaylor@gmail.com>
2019-04-03 11:23:09 -0400

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Task } from './models/task.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title: string = 'Angular To Do List';
+  currentFocus: string = 'angular homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  
+  tasks: Task[] = [
+    new Task(`epicodus homework ${this.month}`, 3),
+    new Task(`do the laundry ${this.day}`, 2),
+    new Task(`test to do task ${this.year}`, 2)
+  ];
+
+  editTask() {
+    alert(`time to edit task`);
+  }
+
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+}
+
